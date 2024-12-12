54b7b0d8749015f9dcf59766a5c6a5684cde087c
TOKY <>
2024-12-12 13:21:50 +0100

    perf: register models

--- base_app/admin.py
+++ base_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Employee, Email, Mail, Receiver
+
+admin.site.register(Employee)
+admin.site.register(Email)
+admin.site.register(Mail)
+admin.site.register(Receiver)
