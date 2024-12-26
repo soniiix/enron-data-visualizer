86c20be33b311a4e80f9821e9cbe60b37958cd88
soniiix <quent.couzinet@gmail.com>
2024-12-26 18:48:03 +0100

    chore: rename template files to english

--- base_app/views.py
+++ base_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "accueil.html")
+    return render(request, "home.html")
 
 def people(request):
-    return render(request, "personnes.html")
+    return render(request, "people.html")
 
 def mails(request):
     return render(request, "mails.html")
 
 def favorites(request):
-    return render(request, "favoris.html")
+    return render(request, "favorites.html")
