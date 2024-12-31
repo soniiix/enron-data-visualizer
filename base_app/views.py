ae69746a72c9f2008c377df7dc5ccfec857beae7
soniiix <quent.couzinet@gmail.com>
2024-12-31 17:11:44 +0100

    feat: retrieve main stats and top 3 people and display them

--- base_app/views.py
+++ base_app/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from base_app.models import Mail, Employee, Email
+from datetime import datetime
+from django.db.models import Count
 
 def home(request):
-    return render(request, "home.html")
+    CURRENT_YEAR = datetime.today().year
+    YEARS_RANGE = ["1900", CURRENT_YEAR]
+    
+    # Nombre total de mails
+    email_count = Mail.objects.count()
+    # Nombre total de personnes?
+    people_count = Employee.objects.count()
+    # Récupérer la période couverte
+    min_year = Mail.objects.filter(date_mail__year__range=YEARS_RANGE).earliest("date_mail").date_mail.year
+    max_year = Mail.objects.filter(date_mail__year__range=YEARS_RANGE).latest("date_mail").date_mail.year
+    covered_period = f"{min_year} - {max_year}"
+    # Trouver les 3 personnes les plus actives (en fonction du nombre de mails envoyés)
+    top3_people = (Employee.objects.annotate(email_count=Count('email__mail')).order_by('-email_count')[:3])
+
+    context = {
+        "email_count": format(email_count, ',').replace(',', ' '),
+        "people_count": format(people_count, ',').replace(',', ' '),
+        "covered_period": covered_period,
+        "top3_people": top3_people
+    }
+    return render(request, "home.html", context)
 
 def people(request):
     return render(request, "people.html")
