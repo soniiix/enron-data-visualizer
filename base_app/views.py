1ccff6695fd2f897aa65af02917a3803f06a0f26
tokyandria001 <>
2025-01-08 12:46:49 +0100

    feat: add a table of people with a filter

--- base_app/views.py
+++ base_app/views.py
@@ -45,7 +45,21 @@ def home(request):
 
 
 def people(request):
-    return render(request, "people.html")
+    category_filter = request.GET.get('category', None)
+
+    if category_filter:
+        employees = Employee.objects.filter(category=category_filter)
+    else:
+        employees = Employee.objects.all()
+        
+    categories = Employee.objects.values_list('category', flat=True).distinct()
+
+    context = {
+        'employees': employees,
+        'categories': categories,
+    }
+
+    return render(request, 'people.html', context)
 
 
 def mails(request):
